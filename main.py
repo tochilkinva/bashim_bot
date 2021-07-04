530eb73d3b75b8c090125be0bd4b02b0a14be4df
Valentin Tochilkin <foxairman@gmail.com>
2021-07-04 14:39:48 +0300

    Added working time, cmd help, rand, start

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ import requests
 import telegram
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://bash.im/'
+URL_RAND = 'https://bash.im/random'
+
+# Формат часа от 0 до 23
+BOT_TIME_START = int(10)
+BOT_TIME_END = int(22)
 
 last_quote_number = 0
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
@@ -91,6 +97,7 @@ def parse_quotes(raw_text):
 
 
 def send_message(message):
+    """Функция бота для отправки сообщения в чат"""
     logging.info('Сообщение отправлено')
     return bot.send_message(CHAT_ID, message)
 
@@ -98,15 +105,27 @@ def send_message(message):
 def main():
     logging.debug('Запущен главный цикл бота')
     while True:
+        if not working_time():
+            logging.info('Не время для работы')
+            time.sleep(60 * 60)  # Перерыв на 1 час
+            continue
+
         try:
             response = request_site(URL)
             quotes = parse_quotes(response)
-            if quotes:
-                new_quotes = find_new_quotes(quotes)
-                if new_quotes:
-                    for key, value in new_quotes.items():
-                        send_message(f'Цитата: {value}')
+            if not quotes:
+                logging.debug('Нет новых цитат')
+                time.sleep(5 * 60)  # Опрашивать раз в пять минут
+                continue
 
+            new_quotes = find_new_quotes(quotes)
+            if not new_quotes:
+                logging.debug('Нет новых цитат')
+                time.sleep(5 * 60)  # Опрашивать раз в пять минут
+                continue
+
+            for value in new_quotes.values():
+                send_message(value)
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
@@ -114,6 +133,36 @@ def main():
             logging.error(f'Бот упал с ошибкой: {e}')
             send_message(f'Бот упал с ошибкой: {e}')
             time.sleep(5 * 60)
+
+
+def get_rand_quotes():
+    """Получаем случайные цитаты"""
+    logging.debug('Получаем случайные цитаты')
+    while True:
+        try:
+            response = request_site(URL_RAND)
+            quotes = parse_quotes(response)
+            return quotes
+
+        except Exception as e:
+            print(f'Бот упал с ошибкой: {e}')
+            logging.error(f'Бот упал с ошибкой: {e}')
+            send_message(f'Бот упал с ошибкой: {e}')
+
+
+def rand_quotes(update, context):
+    """После команды /rand отправляет случайные цитаты"""
+    logging.debug('Отправляем случайные цитаты')
+    chat = update.effective_chat
+    quotes = get_rand_quotes()
+    if quotes:
+        logging.info('Сообщение отправлено')
+        context.bot.send_message(chat_id=chat.id, text='Случайные цитаты')
+        for value in quotes.values():
+            send_message(value)
+    else:
+        logging.info('Сообщение отправлено')
+        context.bot.send_message(chat_id=chat.id, text='Ой, цитаты кончились!')
 
 
 def test():
@@ -136,8 +185,40 @@ def test():
         send_message(value)
 
 
+def working_time():
+    """Определяем может ли бот отправлять сообщения в это время"""
+    time_now = time.localtime()
+    if BOT_TIME_END > time_now.tm_hour > BOT_TIME_START:
+        return True
+
+    return False
+
+
+def wake_up(update, context):
+    """Сообщение при запуске бота"""
+    logging.debug('Отправляем сообщение при запуске бота')
+    chat = update.effective_chat
+    text = 'Бот для получения новых цитат с bash.im запущен!'
+    context.bot.send_message(chat_id=chat.id, text=text)
+
+
+def help_cmd(update, context):
+    """Сообщение после команды /help"""
+    logging.debug('Отправляем сообщение после команды /help')
+    chat = update.effective_chat
+    text = 'Бот автоматически отправляет новые цитаты.'
+    text += ' Для получения случайных цитат отправьте /rand'
+    context.bot.send_message(chat_id=chat.id, text=text)
+
+
 if __name__ == '__main__':
     logging.debug('Бот запущен')
-    # last_quote_number = 466155
+    updater = Updater(token=TELEGRAM_TOKEN)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('help', help_cmd))
+    updater.dispatcher.add_handler(CommandHandler('rand', rand_quotes))
+    updater.start_polling()
+
+    # last_quote_number = 466161
     main()
     # test()
